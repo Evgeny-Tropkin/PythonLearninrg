b1134cf21ab21d5ef82aa9e981f7ff8ff02f9d66
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-18 11:41:46 +0300

    Topics/Lambda functions/Re-write using lambda functions/ problem solved

--- Topics/Lambda functions/Re-write using lambda functions/main.py
+++ Topics/Lambda functions/Re-write using lambda functions/main.py
@@ -3,4 +3,4 @@ b = 2
 c = 3
 
 # Don't forget to make use of lambda functions in your solution
-result = (lambda ...)(a, b, c)
+result = (lambda param1, param2, param3: (param1 + param2) * param3)(a, b, c)
