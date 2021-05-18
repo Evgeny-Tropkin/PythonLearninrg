a7a4605e4622499454a1cb274635c344cffa2e47
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-18 11:14:37 +0300

    Topics/For loop/Speech generation/ problem solved

--- Topics/For loop/Speech generation/main.py
+++ Topics/For loop/Speech generation/main.py
@@ -1,1 +1,5 @@
+phone_number = input()
+number_aloud = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
+for digit in phone_number:
+    print(number_aloud[int(digit)])
