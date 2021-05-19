7f3558c3cdbdaa959ad95f1732505ea1c022f0a3
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-19 08:27:28 +0300

    Topics/Map and filter/Re-write using map( ) and filter( )/: problem solved

--- Topics/Map and filter/Re-write using map( ) and filter( )/main.py
+++ Topics/Map and filter/Re-write using map( ) and filter( )/main.py
@@ -1,9 +1,8 @@
 even = [0, 2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-# Re-write the rest of the code here using map() and filter() where possible
-my_sum = ...
+my_sum = list(map(lambda x, y: x + y, even, odd))
 
-remainders = ...
+remainders = list(map(lambda x: x % 3, my_sum))
 
-nonzero_remainders = ...
+nonzero_remainders = list(filter(lambda r: r, remainders))
