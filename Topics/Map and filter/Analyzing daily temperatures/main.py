bd010e2a043178ac8c30c44ceea9884e6e93e8b3
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-19 08:46:19 +0300

    Topics/Map and filter/Analyzing daily temperatures/: problem solved

--- Topics/Map and filter/Analyzing daily temperatures/main.py
+++ Topics/Map and filter/Analyzing daily temperatures/main.py
@@ -7,8 +7,8 @@ daily_temp_c = [20.5, 19, 15, 25, 27, 30, 31, 29, 26, 21,
                 18, 17.5, 17, 16.5, 19, 20, 25, 26.5, 27, 28,
                 20.5, 19, 25, 27.5, 28, 26, 15, 25, 27, 28]
 
-daily_temp_f = list(...)
+daily_temp_f = list(map(celsius_to_fahrenheit, daily_temp_c))
 
-temp_above_80 = list(...)
+temp_above_80 = list(filter(lambda t: t > 80, daily_temp_f))
 
 print(len(temp_above_80))
