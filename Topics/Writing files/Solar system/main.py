b38433fc99cdb658250a6033a133d4b3ce29c359
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-19 22:25:46 +0300

    Topics/Writing files/Solar system/: problem solved

--- Topics/Writing files/Solar system/main.py
+++ Topics/Writing files/Solar system/main.py
@@ -1,1 +1,9 @@
 # create the planets.txt
+planets_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets_file = open("planets.txt", "w", encoding="utf-8")
+
+for planet_name in planets_list:
+    planets_file.write(planet_name)
+    planets_file.write("\n")
+
+planets_file.close()
