5ce723b172b3de4bc700ae6aa01474c7e8718f2f
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-19 22:51:08 +0300

    Topics/Writing files/Animals/: problem solved

--- Topics/Writing files/Animals/main.py
+++ Topics/Writing files/Animals/main.py
@@ -1,2 +1,11 @@
 # read animals.txt
+animals_file = open("animals.txt", "r")
+animals = animals_file.readlines()
+animals_new = list(map(lambda animal: animal.replace("\n", " "), animals))
+
 # and write animals_new.txt
+animals_new_file = open("animals_new.txt", "w")
+animals_new_file.writelines(animals_new)
+
+animals_file.close()
+animals_new_file.close()
