83d37c1c535ac9c2c8565ddfa7e5aee628ba2736
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-19 04:41:25 +0300

    Topics/Default arguments/Markdown heading/: problem solved

--- Topics/Default arguments/Markdown heading/main.py
+++ Topics/Default arguments/Markdown heading/main.py
@@ -1,2 +1,7 @@
-def heading():
-    pass
+def heading(text, level=1):
+    if level < 1:
+        level = 1
+    if level > 6:
+        level = 6
+    hash_tags = "#" * level
+    return f"{hash_tags} {text}"
