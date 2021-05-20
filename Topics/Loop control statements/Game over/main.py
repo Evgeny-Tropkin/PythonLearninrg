db63fae543b230dab8f970ce06a81403c173bf13
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-20 15:19:15 +0300

    Topics/Loop control statements/Game over/: problem solved

--- Topics/Loop control statements/Game over/main.py
+++ Topics/Loop control statements/Game over/main.py
@@ -1,2 +1,16 @@
 scores = input().split()
-# put your python code here
+correct = 0
+incorrect = 0
+
+for answer in scores:
+    if answer == "C":
+        correct += 1
+    if answer == "I":
+        incorrect += 1
+    if incorrect > 2:
+        print("Game over")
+        break
+else:
+    print("You won")
+
+print(correct)
