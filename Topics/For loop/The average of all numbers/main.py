4f16e52c79f4b43f2d9212863e2d602d900a473a
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-20 08:31:44 +0300

    Topics/For loop/The average of all numbers/: problen solved

--- Topics/For loop/The average of all numbers/main.py
+++ Topics/For loop/The average of all numbers/main.py
@@ -1,1 +1,5 @@
-# put your python code here
+start_num = int(input())
+end_num = int(input())
+
+divided_by_3 = list(filter(lambda num: num % 3 == 0, range(start_num, end_num + 1)))
+print(sum(divided_by_3)/len(divided_by_3))
