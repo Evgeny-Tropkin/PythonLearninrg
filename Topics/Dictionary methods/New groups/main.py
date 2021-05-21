33326925386d5188b74aaadc6aa0ba27bbabf585
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-21 11:31:22 +0300

    Topics/Dictionary methods/New groups/: problem solved

--- Topics/Dictionary methods/New groups/main.py
+++ Topics/Dictionary methods/New groups/main.py
@@ -1,4 +1,10 @@
 # the list with classes; please, do not modify it
 groups = ['1A', '1B', '1C', '2A', '2B', '2C', '3A', '3B', '3C']
+number_of_groups = int(input())
+groups_data = dict.fromkeys(groups)
 
-# your code here
+for i in range(number_of_groups):
+    number_of_children = int(input())
+    groups_data[groups[i]] = number_of_children
+
+print(groups_data)
