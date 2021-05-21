bf9ec09de6099b314fa35c7c0d34f7c3ed5256c8
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-21 12:19:55 +0300

    Topics/Dictionary methods/Big family/: problem solved

--- Topics/Dictionary methods/Big family/main.py
+++ Topics/Dictionary methods/Big family/main.py
@@ -3,3 +3,7 @@ first_family = json.loads(input())
 second_family = json.loads(input())
 
 # Work with the 'first_family' and 'second_family' and create a new dictionary
+new_family = {}
+new_family.update(first_family)
+new_family.update(second_family)
+print(new_family)
