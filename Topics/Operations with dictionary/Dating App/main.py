0dbcb26c0cd6fdcfadb34073c6ce5ee074551939
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-23 19:28:56 +0300

    Topics/Operations with dictionary/Dating App: problem solved

--- Topics/Operations with dictionary/Dating App/main.py
+++ Topics/Operations with dictionary/Dating App/main.py
@@ -1,2 +1,8 @@
 def select_dates(potential_dates):
-    ...
+    desired_age = 30
+    desired_hobby = "art"
+    desired_city = "Berlin"
+    result = [person["name"] for person in potential_dates if (person["age"] > desired_age
+                                                               and desired_hobby in person["hobbies"]
+                                                               and person["city"] == desired_city)]
+    return ", ".join(result)
