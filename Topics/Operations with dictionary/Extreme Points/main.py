5d0a241bcbc95a74886aaa7bd2b0fc3fab6a464e
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-24 09:32:17 +0300

    modified:   Topics/Operations with dictionary/Extreme Points/: problem solved

--- Topics/Operations with dictionary/Extreme Points/main.py
+++ Topics/Operations with dictionary/Extreme Points/main.py
@@ -2,3 +2,19 @@
 test_dict = json.loads(input())
 
 # Work with the 'test_dict'
+any_key = list(test_dict.keys())[0]
+max_value_key = any_key
+max_value = test_dict[max_value_key]
+min_value_key = any_key
+min_value = test_dict[min_value_key]
+
+for key, value in test_dict.items():
+    if value > max_value:
+        max_value = value
+        max_value_key = key
+    elif value < min_value:
+        min_value = value
+        min_value_key = key
+
+print(f"min: {min_value_key}")
+print(f"max: {max_value_key}")
