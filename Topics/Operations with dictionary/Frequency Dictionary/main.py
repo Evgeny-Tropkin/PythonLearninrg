4e151e480bc47da38cf1ba6e70ca4db66a4b71d5
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-24 10:57:37 +0300

    Topics/Operations with dictionary/Frequency Dictionary/: problem solved

--- Topics/Operations with dictionary/Frequency Dictionary/main.py
+++ Topics/Operations with dictionary/Frequency Dictionary/main.py
@@ -1,1 +1,8 @@
-# put your python code here
+input_str = input().split(" ")
+result = {word.lower(): 0 for word in input_str}
+
+for word in input_str:
+    result[word.lower()] += 1
+
+for k, v in result.items():
+    print(f"{k} {v}")
