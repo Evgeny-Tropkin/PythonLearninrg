79361ba4b9ba92dd60f46edf32b8e2b9bdeac98c
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-25 14:52:16 +0300

    Memorization Tool/Make your own flashcards/: stage implemented
    "Make your own flashcards" is stage 1/4 of "Memorization Tool" project

--- Memorization Tool/task/tool.py
+++ Memorization Tool/task/tool.py
@@ -1,8 +1,9 @@
-menu = {"1": {"title": "Add flashcards", "1": {"title": "Add new flashcard"}, "2": {"title": "Exit"}},
+menu = {"1": {"title": "Add flashcards", "1": {"title": "Add a new flashcard"}, "2": {"title": "Exit"}},
         "2": {"title": "Practice flashcards"},
         "3": {"title": "Exit"}}
 menu_path = []
 current_menu_level = menu
+flashcards = {}
 
 
 # region Methods
@@ -14,25 +15,51 @@ def show_menu(menu_dict):
 
 
 def select_menu_item(menu_dict):
-    while True:
-        selected_item_number = input()
-        if selected_item_number in menu_dict.keys():
-            return selected_item_number
-        else:
-            print(f"{selected_item_number} is not an option")
+    selected_item_number = input()
+    if selected_item_number in menu_dict.keys():
+        return selected_item_number
+    else:
+        print(f"{selected_item_number} is not an option")
+        return None
 
 
 def execute_selected_item(menu_dict, item):
     global current_menu_level
-    if menu_dict[item]['title'] == "Exit":
+    current_item = menu_dict[item]['title']
+    if current_item == "Exit":
         if len(menu_path) > 0:
             current_menu_level = menu_path.pop()
         else:
             __exit__()
+    elif current_item == "Add a new flashcard":
+        add_flashcard()
+    elif current_item == "Practice flashcards":
+        start_practice()
 
 
-def add_flashcards():
-    pass
+def add_flashcard():
+    question = ""
+    answer = ""
+    while question == "":
+        question = input("Question:")
+    while answer == "":
+        answer = input("Answer:")
+    flashcards.update({question: answer})
+
+
+def start_practice():
+    if len(flashcards.keys()) == 0:
+        print("There is no flashcard to practice!")
+        return
+
+    for question, answer in flashcards.items():
+        print(f"Question: {question}")
+        is_show_answer = input('Please press "y" to see the answer or press "n" to skip:').lower()
+        while is_show_answer not in ['y', 'n']:
+            print(f"{is_show_answer} is not an option")
+            is_show_answer = input('Please press "y" to see the answer or press "n" to skip:').lower()
+        if is_show_answer == 'y':
+            print(f"Answer: {answer}")
 
 
 def __exit__():
@@ -45,6 +72,8 @@ def __exit__():
 while True:
     show_menu(current_menu_level)
     selected_menu_item = select_menu_item(current_menu_level)
+    if selected_menu_item is None:
+        continue
     if len(current_menu_level[selected_menu_item]) > 1:
         menu_path.append(current_menu_level)
         current_menu_level = current_menu_level[selected_menu_item]
