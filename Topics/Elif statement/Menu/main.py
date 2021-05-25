dc599fea7f0d83da159d71db0558e2447c1754f7
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-25 15:12:03 +0300

    Topics/Elif statement/Menu/: task added and problem solved

---
+++ Topics/Elif statement/Menu/main.py
@@ -0,0 +1,10 @@
+kind_of_food = input()
+
+if kind_of_food == "pizza":
+    print("Margherita, Four Seasons, Neapolitan, Vegetarian, Spicy")
+elif kind_of_food == "salad":
+    print("Caesar salad, Green salad, Tuna salad, Fruit salad")
+elif kind_of_food == "soup":
+    print("Chicken soup, Ramen, Tomato soup, Mushroom cream soup")
+else:
+    print("Sorry, we don't have it in the menu")
