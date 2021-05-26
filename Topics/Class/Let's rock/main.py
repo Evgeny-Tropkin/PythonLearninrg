0f20519a6dadc11c3e2d115ed6f3021b97f273b4
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-26 17:07:21 +0300

    Topics/Class/Let's rock/: problem solved

--- Topics/Class/Let's rock/main.py
+++ Topics/Class/Let's rock/main.py
@@ -1,1 +1,10 @@
-# start a RockBand here
+class RockBand:
+    genre = "rock"
+    key_instruments = ["electric guitar", "drums"]
+    n_members = 4
+
+
+r_band = RockBand()
+print(r_band.genre)
+print(r_band.n_members)
+print(r_band.key_instruments)
