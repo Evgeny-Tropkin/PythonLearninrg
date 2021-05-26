cf149e4fd596ea9be3401ea2c98d560e3932e562
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-26 16:33:16 +0300

    Topics/Class/Who is who/: problem solved

--- Topics/Class/Who is who/main.py
+++ Topics/Class/Who is who/main.py
@@ -8,3 +8,14 @@ class Demon:
     color = "red"
     feature = "horns"
     home = "Hell"
+
+
+example_of_angel = Angel()
+example_of_demon = Demon()
+
+print(example_of_angel.color)
+print(example_of_angel.feature)
+print(example_of_angel.home)
+print(example_of_demon.color)
+print(example_of_demon.feature)
+print(example_of_demon.home)
