1a71d55a32009e69361c3aa42910fd881a21f3ec
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-27 11:02:47 +0300

    Topics/Methods/Hexagon/: problem solved

--- Topics/Methods/Hexagon/main.py
+++ Topics/Methods/Hexagon/main.py
@@ -5,5 +5,6 @@ class Hexagon:
     def __init__(self, side_length):
         self.side_length = side_length
 
-
-    # create get_area here
+    def get_area(self):
+        area = 3 * math.sqrt(3) * self.side_length * self.side_length / 2
+        return round(area, 3)
