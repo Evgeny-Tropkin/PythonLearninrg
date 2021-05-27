38309985bb98cb48a5430d64ff55de6078907b90
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-27 08:30:35 +0300

    Topics/Methods/Greetings/: problem solved

--- Topics/Methods/Greetings/main.py
+++ Topics/Methods/Greetings/main.py
@@ -2,4 +2,11 @@ class Person:
     def __init__(self, name):
         self.name = name
 
-    # create the method greet here
+    def greet(self):
+        return "Hello, I am " + self.name + "!"
+
+
+entered_name = input()
+
+new_person = Person(entered_name)
+print(new_person.greet())
