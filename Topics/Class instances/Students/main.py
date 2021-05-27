0de14b934d64ccf9bb277521c69bc349bdff1bde
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-27 05:31:17 +0300

    Topics/Class instances/Students/: problem solved

--- Topics/Class instances/Students/main.py
+++ Topics/Class instances/Students/main.py
@@ -4,4 +4,16 @@ class Student:
         self.name = name
         self.last_name = last_name
         self.birth_year = birth_year
-        # calculate the student_id here
+        # calculate the student_id
+        self.student_id = self.name[0] + self.last_name + str(self.birth_year)
+
+    def print_id(self):
+        print(self.student_id)
+
+
+entered_name = input()
+entered_last_name = input()
+entered_birth_year = int(input())
+
+student = Student(entered_name, entered_last_name, entered_birth_year)
+student.print_id()
