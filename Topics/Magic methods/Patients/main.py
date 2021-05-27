3c19588527d19f24c57faacc4705cbb92d78867e
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-27 16:50:55 +0300

    Topics/Magic methods/Patients/: problem solved

--- Topics/Magic methods/Patients/main.py
+++ Topics/Magic methods/Patients/main.py
@@ -4,4 +4,8 @@ class Patient:
         self.last_name = last_name
         self.age = age
 
-    # create methods here
+    def __repr__(self):
+        return f"Object of the class Patient. name: {self.name}, last_name: {self.last_name}, age: {self.age}"
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}. {self.age}"
