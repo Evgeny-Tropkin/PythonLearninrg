a91a105e70e61c956aaed6ae68bc359668bfc29b
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-27 11:11:21 +0300

    Topics/Methods/Point/: problem solved

--- Topics/Methods/Point/main.py
+++ Topics/Methods/Point/main.py
@@ -1,2 +1,11 @@
+import math
+
+
 class Point:
-    pass
+    def __init__(self, coord_x, coord_y):
+        self.x = coord_x
+        self.y = coord_y
+
+    def dist(self, point):
+        distance = math.sqrt((self.x - point.x) ** 2 + (self.y - point.y) ** 2)
+        return distance
