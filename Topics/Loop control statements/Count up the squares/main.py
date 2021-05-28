c13e0a4f080ecba56edd7699c11b84eee74eef8d
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-28 11:03:44 +0300

    Topics/Loop control statements/Count up the squares/: problem solved

--- Topics/Loop control statements/Count up the squares/main.py
+++ Topics/Loop control statements/Count up the squares/main.py
@@ -1,1 +1,11 @@
-# put your python code here
+sum_of_numbers = 0
+sum_of_squares = 0
+
+while True:
+    num = int(input())
+    sum_of_numbers += num
+    sum_of_squares += num * num
+    if sum_of_numbers == 0:
+        break
+
+print(sum_of_squares)
