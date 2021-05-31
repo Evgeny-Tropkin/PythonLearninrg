e9943269807b91ad8469b50e2ac139b2d8e40e9e
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-05-31 11:52:33 +0300

    Topics/Nested lists/Word list/: problem solved

--- Topics/Nested lists/Word list/main.py
+++ Topics/Nested lists/Word list/main.py
@@ -1,3 +1,7 @@
 text = [["Glitch", "is", "a", "minor", "problem", "that", "causes", "a", "temporary", "setback"],
         ["Ephemeral", "lasts", "one", "day", "only"],
         ["Accolade", "is", "an", "expression", "of", "praise"]]
+
+max_word_length = int(input())
+
+print([word for any_set in text for word in any_set if len(word) <= max_word_length])
