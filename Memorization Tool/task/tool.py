6215a3a88d8b2859dc0861541b2e4e41d5e426e8
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-06-11 23:27:22 +0300

    Memorization Tool/Update the flashcards(Stage3/4): old implementation deleted
    Main method code deleted
    show_menu() method code deleted
    execute_selected_item() method code deleted
    select_menu_item() method code deleted

--- Memorization Tool/task/tool.py
+++ Memorization Tool/task/tool.py
@@ -58,22 +58,7 @@ item_2_u.nodes = {'e': item_2_u_e, 'd': item_2_u_d}
 # endregion
 # endregion
 
-
-menu = {"1": {"title": "Add flashcards", "is_executed": False,
-              "1": {"title": "Add a new flashcard"},
-              "2": {"title": "Exit"}
-              },
-        "2": {"title": "Practice flashcards", "is_executed": True,
-              "y": {"title": 'press "y" to see the answer:'},
-              "n": {"title": 'press "n" to skip:'},
-              "u": {"title": 'press "u" to update:',
-                    "d": {"title": 'press "d" to delete the flashcard:'},
-                    "e": {"title": 'press "e" to edit the flashcard:'}
-                    }
-              },
-        "3": {"title": "Exit"}}
-menu_path = []
-current_menu_level = menu
+current_menu_level = root
 # endregion
 
 
@@ -86,34 +71,16 @@ def connect_to_sqlite_db(connection_string):
     return Session()
 
 
-def show_menu(menu_dict):
-    for menu_item in menu_dict.keys():
-        if menu_item == "title":
-            continue
-        print(f"{menu_item}. {menu_dict[menu_item]['title']}")
+def show_menu():
+    pass
 
 
-def select_menu_item(menu_dict):
-    selected_item_number = input()
-    if selected_item_number in menu_dict.keys():
-        return selected_item_number
-    else:
-        print(f"{selected_item_number} is not an option")
-        return None
+def select_menu_item():
+    pass
 
 
-def execute_selected_item(menu_dict, item, session_obj):
-    global current_menu_level
-    current_item = menu_dict[item]['title']
-    if current_item == "Exit":
-        if len(menu_path) > 0:
-            current_menu_level = menu_path.pop()
-        else:
-            __exit__()
-    elif current_item == "Add a new flashcard":
-        add_flashcard(session_obj)
-    elif current_item == "Practice flashcards":
-        start_practice(session_obj)
+def execute_selected_item():
+    pass
 
 
 def add_flashcard(session_obj):
@@ -154,14 +121,6 @@ def __exit__():
 # region Main()
 session = connect_to_sqlite_db("sqlite:///flashcard.db?check_same_thread=False")
 while True:
-    show_menu(current_menu_level)
-    selected_menu_item = select_menu_item(current_menu_level)
-    if selected_menu_item is None:
-        continue
-    if len(current_menu_level[selected_menu_item]) > 1:
-        menu_path.append(current_menu_level)
-        current_menu_level = current_menu_level[selected_menu_item]
-        continue
-    print()
-    execute_selected_item(current_menu_level, selected_menu_item, session)
+    show_menu()
+
 # endregion
