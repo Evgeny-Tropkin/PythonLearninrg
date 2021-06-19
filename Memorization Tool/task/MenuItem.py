05e20d90ca185a2d2359882847a2c8a45096f408
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-06-19 06:03:52 +0300

    Memorization Tool/Update the flashcards(Stage3/4): MenuItem class updated
    All fields have been replaced with private ones

--- Memorization Tool/task/MenuItem.py
+++ Memorization Tool/task/MenuItem.py
@@ -1,6 +1,6 @@
 class MenuItem:
     def __init__(self, menu_id, parent, title):
-        self.menu_id = menu_id
-        self.parent = parent
-        self.title = title
-        self.nodes = {}
+        self.__menu_id = menu_id
+        self.__parent = parent
+        self.__title = title
+        self.__nodes = {}
