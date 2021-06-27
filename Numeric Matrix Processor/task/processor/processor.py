4449c7f213722383b03d7d3696892d886fb97d6a
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-06-27 14:20:36 +0300

    Numeric Matrix Processor/Addition(stage 1/6): class CustomMatrix added
    - __init__() method added
    - get_rows() method added and implemented
    - get_columns() method added and implemented
    - set_value() method added without implementation
    _ __add__ method (dunder) added wihout implementation

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,3 +1,25 @@
+# region Classes
+class CustomMatrix:
+    def __init__(self, rows, columns):
+        self.__rows = rows
+        self.__columns = columns
+        self.set_value()
+
+    def get_rows_count(self):
+        return self.__rows
+
+    def get_columns_count(self):
+        return self.__columns
+
+    def set_value(self):
+        pass
+
+    def __add__(self, other):
+        if other.get_rows() == self.__rows and other.get_columns() == self.__columns:
+            pass
+
+# end region
+
 # region Methods
 def main():
     matrix1 = input_matrix()
@@ -11,6 +33,9 @@ def main():
 
 
 def input_matrix():
+    """ The function is intended for entering a matrix of a given dimension. \n
+        Returns a List with elements of the List type. \n
+        The number of nesting levels depends on the specified dimension of the matrix"""
     return []
 # endregion
 
