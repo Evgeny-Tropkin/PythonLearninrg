bbfb662f8b349a5a5fe15822dc7aec4acee7e4db
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-06-30 22:00:45 +0300

    Numeric Matrix Processor/Transpose(stage 4/6): stage implemented
    - CustomMatrix calss updated: transpose_over_horizontal_line() method fixed

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -99,7 +99,7 @@ class CustomMatrix:
     def transpose_over_horizontal_line(self):
         res = CustomMatrix(self.__rows, self.__columns)
         for row_num in range(1, self.__rows + 1):
-            res.set_row(row_num * (-1), self.get_row(row_num))
+            res.set_row(row_num * (-1) + 1, self.get_row(row_num))
         return res
 
     def __add__(self, other):
