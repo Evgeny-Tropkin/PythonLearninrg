ec9060936a71eb9c9f20b5e7a6deffedc14edc0d
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-07 17:46:10 +0300

    Topics/Math magic methods/Largest city/: problem solved

--- Topics/Math magic methods/Largest city/main.py
+++ Topics/Math magic methods/Largest city/main.py
@@ -1,2 +1,7 @@
 class City:
-    # define methods here
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def __gt__(self, other):
+        return self.population > other.population
