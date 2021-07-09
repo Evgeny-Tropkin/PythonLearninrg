15c2995122c7b925d552b634bc702a8371ceb997
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-09 23:31:50 +0300

    Topics/Split and join/Find positions/: problem solved

--- Topics/Split and join/Find positions/main.py
+++ Topics/Split and join/Find positions/main.py
@@ -1,1 +1,12 @@
-# put your python code here
+number_sequence = [int(num_string) for num_string in input().split()]
+searched_number = int(input())
+positions = []
+
+for item_num, value in enumerate(number_sequence):
+    if value == searched_number:
+        positions.append(str(item_num))
+
+if len(positions) > 0:
+    print(' '.join(positions))
+else:
+    print("not found")
