78e9d39bd38767d9d019d849f418cd059148587c
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-09 23:10:12 +0300

    Topics/Split and join/: problem solved

--- Topics/Split and join/mixedCase/main.py
+++ Topics/Split and join/mixedCase/main.py
@@ -1,1 +1,6 @@
+lower_case_words = input().split()
 
+mixed_case_identifier = lower_case_words[0]
+mixed_case_identifier += ''.join([word.capitalize() for word in lower_case_words[1::]])
+
+print(mixed_case_identifier)
