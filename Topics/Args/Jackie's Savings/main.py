b0d44a3dc51166ed5ab15d003c5b3e4f4e14de7b
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-10 18:11:18 +0300

    Topics/Args/Jackie's Savings/: problem solved

--- Topics/Args/Jackie's Savings/main.py
+++ Topics/Args/Jackie's Savings/main.py
@@ -1,1 +1,6 @@
+def final_deposit_amount(*interest_rates, amount=1000):
+    final_amount = amount
+    for rate in interest_rates:
+        final_amount *= (1 + (rate / 100))
 
+    return round(final_amount, 2)
