15dbe5c1a4cb14430015b2e15fa50e23fc2f1094
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-10 07:44:38 +0300

    Topics/Split and join/Spellchecker/: problem solved

--- Topics/Split and join/Spellchecker/main.py
+++ Topics/Split and join/Spellchecker/main.py
@@ -3,3 +3,12 @@ dictionary = ['all', 'an', 'and', 'as', 'closely', 'correct', 'equivocal',
               'sign', 'the', 'to', 'uncertain']
 
 words = input().split()
+mistake_not_found = True
+for word in words:
+    if word in dictionary:
+        continue
+    else:
+        print(word)
+        mistake_not_found = False
+if mistake_not_found:
+    print("OK")
