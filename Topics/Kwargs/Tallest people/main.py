7370c65aeb7171c774c3bb0e691a44f61f5b447a
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-10 19:34:50 +0300

    Topics/Kwargs/Tallest people/: problem solved

--- Topics/Kwargs/Tallest people/main.py
+++ Topics/Kwargs/Tallest people/main.py
@@ -1,1 +1,7 @@
 def tallest_people(**people):
+    max_height = max(people.values())
+    the_tallest = [key for key, value in people.items() if value == max_height]
+
+    the_tallest.sort()
+    for k in the_tallest:
+        print(f"{k} : {people[k]}")
