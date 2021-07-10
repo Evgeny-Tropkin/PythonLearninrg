f7b49b027d39dc1234ff8bba6e8650c269b50f51
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-10 12:04:21 +0300

    Regex Engine/Single character strings (stage 1/6): stage implemented
    - process_string() method implemented
    - parse_reg_ex() method updated

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,12 +1,22 @@
 def parse_reg_ex(reg_ex_string):
     """The method accepts a string containing a Regular Expression.
        Returns a list of tokens which were found in the entered regular expression """
+    special_characters = ('.',)
     res = []
+    substring = []
 
     if len(reg_ex_string) != 0:
-        for char in reg_ex_string:
-            if char == '.':
+        for pos, char in enumerate(reg_ex_string):
+            if char in special_characters:
+                if len(substring) != 0:
+                    res.append(''.join(substring))
+                    substring.clear()
                 res.append(char)
+                continue
+            substring.append(char)
+        if len(substring) > 0:
+            res.append(''.join(substring))
+        substring.clear()
 
     return res
 
@@ -16,8 +26,34 @@ def process_string(processed_string, reg_ex_list):
         - A regular expression as a list of tokens
         - A string that will be matched against a regular expression
        Returns the result of matching"""
-    res = None
 
+    if len(reg_ex_list) == 0:
+        res = True
+    elif len(processed_string) == 0:
+        res = False
+    else:
+        res = False
+        checking_char_index = 0
+        current_lex_pos = 0
+        while current_lex_pos < len(reg_ex_list):
+            target_item = reg_ex_list[current_lex_pos]
+            if target_item == '.':
+                if not processed_string[checking_char_index].isspace():
+                    checking_char_index += 1
+                    current_lex_pos += 1
+                    continue
+
+            if processed_string.startswith(target_item, checking_char_index):
+                checking_char_index += len(target_item)
+                current_lex_pos += 1
+            else:
+                checking_char_index += 1
+                current_lex_pos = 0
+
+            if checking_char_index >= len(processed_string) and current_lex_pos < len(reg_ex_list):
+                break
+        else:
+            res = True
     return res
 
 
