092b80f453a49ca1f52abd869967a67d2f921412
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-10 07:59:19 +0300

    Topics/Split and join/Straight A's/: problem solved

--- Topics/Split and join/Straight A's/main.py
+++ Topics/Split and join/Straight A's/main.py
@@ -1,1 +1,8 @@
+grades = input().split()
 
+quantity_of_the_best_grade = 0
+for grade in grades:
+    if grade == 'A':
+        quantity_of_the_best_grade += 1
+
+print(round(quantity_of_the_best_grade / len(grades), 2))
