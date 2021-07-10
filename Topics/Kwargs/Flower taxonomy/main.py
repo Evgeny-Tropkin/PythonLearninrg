5e0b59ab2868956b029f3854fb59f25e81f49ace
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-10 19:05:03 +0300

    Topics/Kwargs/Flower taxonomy/: problem solved

--- Topics/Kwargs/Flower taxonomy/main.py
+++ Topics/Kwargs/Flower taxonomy/main.py
@@ -1,1 +1,6 @@
 iris = {}
+
+
+def add_iris(id_n, species, petal_length, petal_width, **features):
+    addition = {id_n: {"species": species, "petal_length": petal_length, "petal_width": petal_width, **features}}
+    iris.update(addition)
