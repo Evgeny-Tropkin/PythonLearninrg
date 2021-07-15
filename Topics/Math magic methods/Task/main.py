c938e7a1920df7d07b448d15b61ca0e6dbaad175
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-15 07:19:03 +0300

    Topics/Math magic methods/Task/: problem solved

--- Topics/Math magic methods/Task/main.py
+++ Topics/Math magic methods/Task/main.py
@@ -3,4 +3,5 @@ class Task:
         self.description = description
         self.team = team
 
-    # create the method
+    def __add__(self, other):
+        return Task(f"{self.description}\n{other.description}", f"{self.team}, {other.team}")
