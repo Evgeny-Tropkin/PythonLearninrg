e6ea9f3eee423ccb73e2cb9c750f18dae5681c05
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-20 22:47:21 +0300

    Topics/Testing user input/Several testings/: problem solved

--- Topics/Testing user input/Several testings/main.py
+++ Topics/Testing user input/Several testings/main.py
@@ -1,2 +1,8 @@
-def check():
-    ...
+def check(q):
+    if not q.isdigit():
+        print("It is not a number!")
+    else:
+        if int(q) >= 202:
+            print(q)
+        else:
+            print("There are less than 202 apples! You cheated on me!")
