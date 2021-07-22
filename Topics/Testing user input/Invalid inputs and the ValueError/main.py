e76d72dcc70e74f40faca10cb8a278fb44980d92
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-07-22 07:21:30 +0300

    Auxiliry updates form the server
    Changes to be committed:
    	modified:   Topics/Dictionary/Theory/task-remote-info.yaml
    	modified:   Topics/Escape sequences/Find the length/task-remote-info.yaml
    	modified:   Topics/Escape sequences/Print a sign/task-remote-info.yaml
    	modified:   Topics/Math functions/lesson-info.yaml
    	modified:   Topics/Split and join/Theory/task-remote-info.yaml
    	modified:   Topics/Testing user input/Checking inputs/main.py
    	modified:   Topics/Testing user input/Invalid inputs and the ValueError/main.py
    	modified:   Topics/Testing user input/Invalid inputs and the ValueError/task-info.yaml
    	modified:   Topics/Testing user input/Several testings/main.py
    	modified:   Topics/Testing user input/lesson-info.yaml
    	modified:   course-remote-info.yaml
    
    Changes not staged for commit:
    	modified:   Regex Engine/task/regex/regex.py

--- Topics/Testing user input/Invalid inputs and the ValueError/main.py
+++ Topics/Testing user input/Invalid inputs and the ValueError/main.py
@@ -1,9 +1,9 @@
 def check():
     try:
-        num = int(input())
-        if 25 <= num <= 37:
-            print(num)
+        number = int(input())
+        if 25 <= number <= 37:
+            print(number)
         else:
-            print("Correct the error!")
+            raise ValueError
     except ValueError:
         print("Correct the error!")
