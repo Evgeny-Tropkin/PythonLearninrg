8201c190460b31f09b6888600735397f8649d65f
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-07 07:03:30 +0300

    Regex Engine/Implementing the operators ^ and $ (stage 4/6): tests for process_string() method on Stage 4 added

--- Regex Engine/task/regex/regex_tests.py
+++ Regex Engine/task/regex/regex_tests.py
@@ -5,7 +5,9 @@ import regex
 
 class TestRegex(unittest.TestCase):
     def test_parse_reg_ex(self):
+        #  tests for Stage 3
         self.assertEqual(regex.parse_reg_ex("a.bc"), ['a', '.', "bc"])
+        #  tests for Stage 4
         self.assertEqual(regex.parse_reg_ex("^abc^"), ['^', "abc^"])
         self.assertEqual(regex.parse_reg_ex("$abc$"), ["$abc", '$'])
         self.assertEqual(regex.parse_reg_ex("^abc$"), ['^', "abc", '$'])
@@ -13,9 +15,18 @@ class TestRegex(unittest.TestCase):
         self.assertEqual(regex.parse_reg_ex("abc$"), ["abc", '$'])
 
     def test_process_string(self):
+        #  tests for Stage 3
         self.assertTrue(regex.process_string("abc", ['.', "bc"]))
         self.assertTrue(regex.process_string("abc", ['a', '.', 'c']))
         self.assertTrue(regex.process_string("abc", ["ab", '.']))
         self.assertTrue(regex.process_string("a bc", ["bc"]))
         self.assertTrue(regex.process_string(" abc", ["bc"]))
         self.assertTrue(regex.process_string("abc ", ["bc"]))
+        #  tests for Stage 4
+        self.assertTrue(regex.process_string("abc", ['^', "ab"]))
+        self.assertTrue(regex.process_string("abc", ["bc", '$']))
+        self.assertTrue(regex.process_string("abc", ['^', "abc", '$']))
+        self.assertTrue(regex.process_string("become", ['^', "be"]))
+        self.assertFalse(regex.process_string("to be", ['^', "be"]))
+        self.assertTrue(regex.process_string("section", ["tion", '$']))
+        self.assertFalse(regex.process_string("sections", ["tion", '$']))
