54f9cceed9cf4142968296e717db32766ea0f141
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-08 07:41:08 +0300

    Regex Engine/Implementing the operators ^ and $ (stage 4/6): process_string() and match() methods updated

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -33,26 +33,28 @@ def process_string(processed_string, reg_ex_list):
         - A regular expression as a list of tokens
         - A string that will be matched against a regular expression
        Returns the result of matching"""
-    res = [False]
+    res = False
 
     if len(reg_ex_list) == 0:
-        res[0] = True
+        res = True
     elif len(processed_string) == 0:
         pass
     else:
-        res[0] = match(processed_string, reg_ex_list, is_startswith=reg_ex_list[0] == '^')
+        is_startswith = reg_ex_list[0] == '^'
 
-    return all(res)
+        if is_startswith:
+            reg_ex_list = reg_ex_list[1::]
+
+        res = match(processed_string, reg_ex_list, is_startswith)
+
+    return res
 
 
-def match(processed_string, reg_ex_list, is_startswith=False, is_endswith=False):
-    res = False
+def match(processed_string, reg_ex_list, is_startswith=False):
+    res = [False]
     checking_char_index = 0
     current_lex_pos = 0
-    if is_startswith:
-        current_lex_pos = 1
-    if is_endswith:
-        pass
+
     while current_lex_pos < len(reg_ex_list):
         target_item = reg_ex_list[current_lex_pos]
         if target_item == '.':
@@ -61,21 +63,26 @@ def match(processed_string, reg_ex_list, is_startswith=False, is_endswith=False)
                 current_lex_pos += 1
                 continue
 
-        if processed_string.startswith(target_item, checking_char_index):
-            checking_char_index += len(target_item)
-            current_lex_pos += 1
-        else:
-            if is_startswith:
+        if checking_char_index >= len(processed_string):
+            if current_lex_pos == len(reg_ex_list) - 1:
+                res.append(reg_ex_list[current_lex_pos] == '$')
+                current_lex_pos += 1
+            elif current_lex_pos < len(reg_ex_list) - 1:
                 break
-            checking_char_index += 1
-            current_lex_pos = 0
+        else:
+            if processed_string.startswith(target_item, checking_char_index):
+                checking_char_index += len(target_item)
+                current_lex_pos += 1
+            else:
+                if is_startswith:
+                    break
+                checking_char_index += 1
+                current_lex_pos = 0
 
-        if checking_char_index >= len(processed_string) and current_lex_pos < len(reg_ex_list):
-            break
     else:
-        res = True
+        res[0] = True
 
-    return res
+    return all(res)
 
 
 def main():
