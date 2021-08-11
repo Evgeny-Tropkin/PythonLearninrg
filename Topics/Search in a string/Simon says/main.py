8bad08aff7299276e44213a49fcea41f2c2575fc
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-11 07:41:22 +0300

    Topics/Search in a string/Simon says/: problem solved

--- Topics/Search in a string/Simon says/main.py
+++ Topics/Search in a string/Simon says/main.py
@@ -1,2 +1,9 @@
 def what_to_do(instructions):
-    ...
+    simon_says = "Simon says"
+    action = "I won't do it!"
+    if instructions.startswith(simon_says):
+        action = "I " + instructions[len(simon_says) + 1::]
+    if instructions.endswith(simon_says):
+        action = "I " + instructions[:len(instructions) - len(simon_says) - 1:]
+
+    return action
