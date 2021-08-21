426191b26ce14bc5eaeda53de7931e1c21fe588b
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-21 07:37:32 +0300

    Topics/Testing user input/PEP 8 conventions/: problem solved

--- Topics/Testing user input/PEP 8 conventions/main.py
+++ Topics/Testing user input/PEP 8 conventions/main.py
@@ -1,2 +1,12 @@
-def check_name(...):
-    ...
+def check_name(variable_name):
+    not_allowed_start_symbols = ['l', 'I', 'O']
+    if variable_name in not_allowed_start_symbols:
+        print("Never use the characters 'l', 'O', or 'I' as single-character variable names")
+        return
+
+    if variable_name.islower():
+        print("It is a common variable")
+    elif variable_name.isupper():
+        print("It is a constant")
+    else:
+        print("You shouldn't use mixedCase")
