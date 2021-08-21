302413423a0ec8576c612a6c4914dbc393b38986
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-21 13:39:09 +0300

    Topics/Regexps in programs/Finding capitalized words/: problem solved

--- Topics/Regexps in programs/Finding capitalized words/main.py
+++ Topics/Regexps in programs/Finding capitalized words/main.py
@@ -2,3 +2,11 @@ import re
 
 string = input()
 # your code here
+capitalized_words_pattern = r"(?<!\w)[A-Z][A-z]*"
+digits_pattern = r"[\d]+"
+
+capitalized_words = re.findall(capitalized_words_pattern, string)
+digits = re.findall(digits_pattern, string)
+
+print("Capitalized words:", ", ".join(capitalized_words))
+print("Digits:", ", ".join(digits))
