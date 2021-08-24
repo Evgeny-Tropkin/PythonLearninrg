b57672606d100c6930157fb8e9b399f58b07367b
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-24 21:17:55 +0300

    Topics/Preconditions and postconditions/Let's calculate the expenses/: problem solved

--- Topics/Preconditions and postconditions/Let's calculate the expenses/main.py
+++ Topics/Preconditions and postconditions/Let's calculate the expenses/main.py
@@ -2,3 +2,9 @@ import re
 
 string = input()
 # your code here
+pattern = r"(?<=\$)(\d+)"
+expenses = re.findall(pattern, string)
+s = 0
+for expense in expenses:
+    s += int(expense)
+print(f"This week you have spent: {s} dollars")
