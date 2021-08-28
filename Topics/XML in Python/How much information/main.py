71d3c2cbdd695f9c8fb708ede328aeb80db720cf
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-28 08:00:22 +0300

    Topics/XML in Python/How much information/: problem solved

--- Topics/XML in Python/How much information/main.py
+++ Topics/XML in Python/How much information/main.py
@@ -1,1 +1,9 @@
 from lxml import etree
+
+xml_string = input()
+
+root = etree.fromstring(xml_string)
+number_of_child_elements = len(root)
+number_of_attributes = len(root.keys())
+
+print(number_of_child_elements, number_of_attributes)
