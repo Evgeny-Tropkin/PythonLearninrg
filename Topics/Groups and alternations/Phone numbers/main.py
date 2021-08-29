9f53d7abe0c939861eef44af057fada5f076552a
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-08-29 09:07:42 +0300

    Topics/Groups and alternations/Phone numbers/: problem solved

--- Topics/Groups and alternations/Phone numbers/main.py
+++ Topics/Groups and alternations/Phone numbers/main.py
@@ -2,3 +2,12 @@ import re
 
 string = input()
 # your code here
+pattern = r"^\+(\d{1})[ -]?(\d{3})[ -]?((\d[ -]?){7})"
+res = re.search(pattern, string)
+if res:
+    print("Full number: ", res.group(), "")
+    print("Country code: ", res.groups()[0], "")
+    print("Area code: ", res.groups()[1], "")
+    print("Number: ", res.groups()[2])
+else:
+    print("No match")
