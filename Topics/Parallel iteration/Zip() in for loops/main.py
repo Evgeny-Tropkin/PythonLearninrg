140fa77f19e592e55d64513e7c3bd6ddc930b572
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-09-15 21:40:17 +0300

    Topics/Parallel iteration/Zip() in for loops/: task added

--- Topics/Parallel iteration/Zip() in for loops/main.py
+++ Topics/Parallel iteration/Zip() in for loops/main.py
@@ -4,3 +4,5 @@ years = [int(x) for x in input().split(',')]
 loan_principles = [int(x) for x in input().split(',')]
 
 # your code here
+for item in zip(interest_rates, years, loan_principles):
+    print(int(item[0] * item[1] * item[2]))
---
+++ Topics/Parallel iteration/Zip() with dictionaries/main.py
@@ -0,0 +1,7 @@
+# please do not modify the following code
+_d_list = [keyword.split(':') for keyword in input().split(', ')]
+domestic_animal = {key: value for key, value in _d_list}
+_w_list = [keyword.split(':') for keyword in input().split(', ')]
+wild_animal = {key: value for key, value in _w_list}
+
+# your code here
