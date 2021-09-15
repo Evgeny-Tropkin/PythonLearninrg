06fa794821f051dbee0d79790c15e89b43726124
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-09-15 21:53:23 +0300

    Topics/Parallel iteration/Zip() with dictionaries/: problem solved

--- Topics/Parallel iteration/Zip() with dictionaries/main.py
+++ Topics/Parallel iteration/Zip() with dictionaries/main.py
@@ -5,3 +5,7 @@ _w_list = [keyword.split(':') for keyword in input().split(', ')]
 wild_animal = {key: value for key, value in _w_list}
 
 # your code here
+zipped = zip(domestic_animal.items(), wild_animal.items())
+for (domestic_key, domestic_value), (wild_key, wild_value) in zipped:
+    print(f"The domestic animal's {domestic_key} is '{domestic_value}'.")
+    print(f"The wild animal's {wild_key} is '{wild_value}'.")
