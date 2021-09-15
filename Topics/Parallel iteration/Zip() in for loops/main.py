4c326932235b0f300a931b11d455c6ab14bb5fb9
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-09-15 21:34:39 +0300

    Topics/Parallel iteration/Zip() in for loops/: task added

---
+++ Topics/Parallel iteration/Zip() in for loops/main.py
@@ -0,0 +1,6 @@
+# please do not modify the following code
+interest_rates = [float(x) for x in input().split(',')]
+years = [int(x) for x in input().split(',')]
+loan_principles = [int(x) for x in input().split(',')]
+
+# your code here
