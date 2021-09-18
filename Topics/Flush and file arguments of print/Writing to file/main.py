911ff527077876ee98be7fede26552213f75f15d
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-09-18 06:51:49 +0300

    Topics/Flush and file arguments of print/Writing to file/: problem solved

--- Topics/Flush and file arguments of print/Writing to file/main.py
+++ Topics/Flush and file arguments of print/Writing to file/main.py
@@ -2,3 +2,6 @@ f_name = "test.txt"
 my_string = "A string to be written to a file!"
 
 # what to do with the file and the string
+my_file = open(f_name, 'w')
+print(my_string, file=my_file)
+my_file.close()
