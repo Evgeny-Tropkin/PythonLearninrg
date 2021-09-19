52b6351a6bf487b7f760386b825328a87f7851a0
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-09-19 19:12:08 +0300

    Topics/Stack in Python/Matching brackets/: problem solved

--- Topics/Stack in Python/Matching brackets/main.py
+++ Topics/Stack in Python/Matching brackets/main.py
@@ -1,1 +1,16 @@
 # put your python code here
+stack = []
+expression = input()
+
+for item in expression:
+    if stack:
+        if stack[-1] == '(' and item == ')':
+            stack.pop()
+            continue
+    if item in ['(', ')']:
+        stack.append(item)
+
+if stack:
+    print("ERROR")
+else:
+    print("OK")
