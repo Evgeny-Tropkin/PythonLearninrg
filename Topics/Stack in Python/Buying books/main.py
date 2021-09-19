c702fc025618f07323c90492ecf40bf49a6f5f5c
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-09-19 17:23:16 +0300

    Topics/Stack in Python/Buying books/: task added

--- Topics/Stack in Python/Buying books/main.py
+++ Topics/Stack in Python/Buying books/main.py
@@ -1,6 +1,6 @@
 count = int(input())
 actions = [None]
-for pos in range(count):
+for _ in range(count):
     current_action = input()
     if current_action.startswith("READ"):
         printed_action = actions.pop()[4::]
