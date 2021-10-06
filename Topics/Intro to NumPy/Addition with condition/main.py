2fd9013a5e7dfb005cea36a0e6b58e1a2a6f7e5c
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-10-06 15:21:20 +0300

    Topics/Intro to NumPy/Addition with condition/: problem solved

--- Topics/Intro to NumPy/Addition with condition/main.py
+++ Topics/Intro to NumPy/Addition with condition/main.py
@@ -1,4 +1,15 @@
 import numpy as np
 
+
 def custom_sum(arg1, arg2):
-    ...
+    arg1_is_list = type(arg1) is list
+    arg2_is_list = type(arg2) is list
+    if arg1_is_list and arg2_is_list:
+        return "Both arguments are lists, not arrays"
+    elif arg1_is_list or arg2_is_list:
+        return "One argument is a list"
+    else:
+        arg1_is_array = type(arg1) is np.ndarray
+        arg2_is_array = type(arg2) is np.ndarray
+        if arg1_is_array and arg2_is_array:
+            return arg1 + arg2
