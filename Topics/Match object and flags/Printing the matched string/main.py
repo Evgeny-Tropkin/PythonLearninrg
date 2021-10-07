1a793b3b6d4230e921438f2de3169b7cff2cdcb2
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2021-10-07 08:38:13 +0300

    Topics/Match object and flags/Printing the matched string/: problem solved

--- Topics/Match object and flags/Printing the matched string/main.py
+++ Topics/Match object and flags/Printing the matched string/main.py
@@ -2,3 +2,10 @@ import re
 
 string = input()
 # your code here
+pattern = "Good (morning|afternoon|evening)"
+
+res = re.match(pattern, string, flags=re.I)
+if res:
+    print(res.group())
+else:
+    print("No greeting!")
